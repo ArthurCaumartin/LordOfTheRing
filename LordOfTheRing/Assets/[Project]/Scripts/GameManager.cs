de451b9efae7f9dabc7ef18d4bedad17e069e3f9
Arthur Caumartin <103814696+ArthurCaumartin@users.noreply.github.com>
2025-09-30 18:28:25 +0200

    asd

--- LordOfTheRing/Assets/[Project]/Scripts/GameManager.cs
+++ LordOfTheRing/Assets/[Project]/Scripts/GameManager.cs
@@ -4,18 +4,22 @@ using UnityEngine.InputSystem;
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance;
+    public string _gameScene = "GameScene";
+    public string _endScene = "EndScene";
+    public int MaxLevel = 5;
     public int CurrentLevel = 0;
 
-    
 
     private void Awake()
     {
         if (Instance != null && Instance != this)
+        {
             Destroy(gameObject);
+            DontDestroyOnLoad(gameObject);
+        }
         else
             Instance = this;
     }
-
 
     public void OnUpLevel(InputValue value)
     {
