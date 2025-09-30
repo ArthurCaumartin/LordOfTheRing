cab0d2f77e10a1fc67e3bf8db62db341fee73950
Arthur Caumartin <103814696+ArthurCaumartin@users.noreply.github.com>
2025-09-30 19:40:43 +0200

    asd

--- LordOfTheRing/Assets/[Project]/Scripts/BallSound.cs
+++ LordOfTheRing/Assets/[Project]/Scripts/BallSound.cs
@@ -8,17 +8,25 @@ public class BallSound : MonoBehaviour
     [SerializeField] private float _minVelocityToPlay = 1f;
 
     private Rigidbody2D _rigidbody2D;
+    private float _lastVelocity;
 
     private void Start()
     {
         _rigidbody2D = GetComponent<Rigidbody2D>();
     }
 
-    private void Update()
+    private void LateUpdate()
     {
-        if (_rigidbody2D.linearVelocity.magnitude > _minVelocityToPlay)
+        _lastVelocity = _rigidbody2D.linearVelocity.magnitude;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        float impactForce = collision.relativeVelocity.magnitude;
+        if (impactForce > _minVelocityToPlay)
         {
-            _audioSource.PlayOneShot(_clips[Random.Range(0, _clips.Count)]);
+            int index = Random.Range(0, _clips.Count);
+            _audioSource.PlayOneShot(_clips[index]);
         }
     }
 }
